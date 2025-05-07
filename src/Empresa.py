d19f3cfa77a6775b8c93e72a224f5eec106a046c
Rafael Vargas <rafael.s.vargas18@gmail.com>
2025-05-07 11:16:03 -0300

    Adicionando código do teste de criar projeto

--- src/Empresa.py
+++ src/Empresa.py
@@ -1,6 +1,16 @@
+from src.Projeto import Projeto 
+
+
 class Empresa():
     def __init__(self):
         self.__projetos = []
 
     def projetos(self):
         return self.__projetos
+    
+    def criar_projeto(self, name) -> Projeto:
+        projeto = Projeto(name)
+        
+        self.__projetos.append(projeto)
+
+        return projeto
---
+++ src/Projeto.py
@@ -0,0 +1,3 @@
+class Projeto:
+    def __init__(self, name):
+        self.name = name
